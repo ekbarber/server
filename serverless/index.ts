c04a9361f1c4c33d3d6465ec53408c6df96e17ae
Eddie <edwardkbarber@gmail.com>
2022-11-01 19:01:31 -0700

    basic layout with user dropdown menu working

--- serverless/index.ts
+++ serverless/index.ts
@@ -5,6 +5,16 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('resources'));
 
-app.get('/', (req, res)=> res.render('index', {title:'Nextcloud Serverless', content:'hi'}));
+app.get('/', (req, res)=> {
+    const userState = {"userId":"eddie","message":null,"messageId":null,"messageIsPredefined":false,"icon":null,"clearAt":null,"status":"online","statusIsUserDefined":false}
+    const data = {
+        title:'Nextcloud Serverless', 
+        content:'hi', 
+        userId:'eddie',
+        userStatusInitialState: Buffer.from(JSON.stringify(userState)).toString('base64'),
+        profileEnabled:Buffer.from(JSON.stringify({"profileEnabled":true})).toString('base64')
+    }
+    res.render('index', data)
+});
 
 app.listen(3000)
