e19e11e6cfa127adc8a6a90bdf5fab1a0d9a0e16
Eddie <edwardkbarber@gmail.com>
2022-11-03 16:39:18 -0700

    simple login working

--- serverless/index.ts
+++ serverless/index.ts
@@ -1,20 +1,63 @@
 import express from 'express';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+
+import Debug from 'debug';
+
+const debug = Debug('nextcloud')
 
 const app = express();
 
+interface User{
+    username: string
+}
+declare module 'express-session' {
+    interface SessionData {
+      user: User;
+    }
+  }
+
+app.use(session({
+    secret:'nextcloud'
+}))
+//app.use()
 app.set('view engine', 'pug');
 app.use(express.static('resources'));
 
+app.use((req, res, next)=>{
+    req.url = req.url.replace('index.php/', '')
+    next()
+})
+
 app.get('/', (req, res)=> {
+    if(!req.session.user){
+        res.redirect('/login')
+        return;
+    }
     const userState = {"userId":"eddie","message":null,"messageId":null,"messageIsPredefined":false,"icon":null,"clearAt":null,"status":"online","statusIsUserDefined":false}
     const data = {
         title:'Nextcloud Serverless', 
-        content:'hi', 
+        content:req.session.user?.username, 
         userId:'eddie',
         userStatusInitialState: Buffer.from(JSON.stringify(userState)).toString('base64'),
         profileEnabled:Buffer.from(JSON.stringify({"profileEnabled":true})).toString('base64')
     }
     res.render('index', data)
 });
+
+app.get('/login', (req, res)=>{
+    const props = {
+        bodyId: 'body-login'
+    }
+    res.render('login', props)
+})
+
+app.post('/login', bodyParser.urlencoded({ extended: false }), (req, res) =>{
+    const user = {
+        username: req.body?.user
+    }
+    req.session.user = user;
+    res.redirect('/')
+})
 
 app.listen(3000)
